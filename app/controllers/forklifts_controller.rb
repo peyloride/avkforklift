59b5f6d15699f06d73575e20d0d0f498586c6fca
Okan Binli <okanb3@gmail.com>
2018-02-12 22:08:42 +0300

    Filtering system implemented

--- app/controllers/forklifts_controller.rb
+++ app/controllers/forklifts_controller.rb
@@ -2,16 +2,24 @@ class ForkliftsController < ApplicationController
   before_action :set_product, only: :show
 
   def index
-    @forklifts = Forklift.order(:created_at).page(params[:page]).per(9)
+    @forklifts = Forklift.order(:index_line_up, :created_at)
+    filtering_params(params).each do |key, value|
+      @forklifts = @forklifts.public_send(key, value) if value.present?
+    end
+    @forklifts = @forklifts.page(params[:page]).per(9)
   end
 
   def show
-    @related_forklifts = Forklift.where(brand: @forklift.brand).limit(10)
+    @related_forklifts = Forklift.brand(@forklift.brand).limit(10)
   end
 
   private
 
   def set_product
     @forklift = Forklift.find(params[:id])
+  end
+
+  def filtering_params(params)
+    params.slice(:forklift_type, :elevator_type, :brand, :lifting_capacity, :lifting_height)
   end
 end
--- app/models/forklift.rb
+++ app/models/forklift.rb
@@ -7,4 +7,25 @@ class Forklift < ApplicationRecord
   enum elevator_type: [:dublex, :triplex]
   enum attachment: [:ss]
   enum status: [:ready, :working, :disassembled, :maintenance, :painting, :sold]
+
+  scope :forklift_type, -> (forklift_type) { where forklift_type: forklift_type }
+  scope :elevator_type, -> (elevator_type) { where elevator_type: elevator_type }
+  scope :brand, -> (brand) { where brand: brand }
+  scope :lifting_capacity, -> (min) { where('lifting_capacity > ? AND lifting_capacity < ?', (min.to_i*1000), (min.to_i+1)*1000) }
+  scope :lifting_height, -> (min) { where('lifting_height > ? AND lifting_height < ?', min.to_i*100, (min.to_i+2)*100) }
+
+  def self.brand_options
+    self.brands.map {|k, v| [k.humanize.titleize, v]}
+  end
+
+  def self.forklift_options
+    self.forklift_types.map {|k, v| [k.humanize.titleize, v]}
+  end
+
+  def self.elevator_options
+    self.elevator_types.map {|k, v| [k.humanize.titleize, v]}
+  end
+
+  def min_max_options
+  end
 end

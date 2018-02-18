2f24e016b90d969d1091376c3e5bc22b16c5ae04
Okan Binli <okanb3@gmail.com>
2018-02-18 15:50:36 +0300

    CKEditor config for Cloudinary

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,7 +1,10 @@
 class Picture < ApplicationRecord
   belongs_to :forklift
 
-  has_attached_file :image, styles: { large: "900x", medium: "300x300>", thumb: "100x100>" },
-                    default_url: "/images/:style/missing.png", storage: :cloudinary, path: ':id/:style/:filename'
+  has_attached_file :image,
+                    styles: { large: "900x", medium: "300x300>", thumb: "100x100>" },
+                    default_url: "/images/:style/missing.png",
+                    storage: :cloudinary,
+                    path: ':id/:style/:filename'
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
